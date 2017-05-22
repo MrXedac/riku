9a407393eef2272badc48307842526030ee98766
Quentin Bergougnoux <quentin.bergougnoux@univ-lille1.fr>
2017-05-22 16:01:15 +0200

    Fixed RAX clobber condition in rLibC

--- libc/include/syscall.h
+++ libc/include/syscall.h
@@ -1,8 +1,8 @@
 #ifndef __SYSCALL__
 #define __SYSCALL__
 
-#define SYSCALL1(ret, id, arg1) __asm volatile("MOV %1, %%RAX; MOV %2, %%RBX; SYSCALL;" : "=a"(ret) : "r"(id), "r"(arg1) : "rbx")
-#define SYSCALL2(ret, id, arg1, arg2) __asm volatile("MOV %1, %%RAX; MOV %2, %%RBX; MOV %3, %%RDX; SYSCALL;" : "=a"(ret) : "r"(id), "r"(arg1), "r"(arg2) : "rbx", "rdx")
-#define SYSCALL3(ret, id, arg1, arg2, arg3) __asm volatile("MOV %1, %%RAX; MOV %2, %%RBX; MOV %3, %%RDX; MOV %4, %%RSI; SYSCALL;" : "=a"(ret) : "r"(id), "r"(arg1), "r"(arg2), "r"(arg3) :"rbx", "rdx", "rsi")
+#define SYSCALL1(ret, id, arg1) __asm volatile("MOV %1, %%RAX; MOV %2, %%RBX; SYSCALL; MOV %%RAX, %0" : "=r"(ret) : "r"(id), "r"(arg1) : "rax", "rbx")
+#define SYSCALL2(ret, id, arg1, arg2) __asm volatile("MOV %1, %%RAX; MOV %2, %%RBX; MOV %3, %%RDX; SYSCALL; MOV %%RAX, %0" : "=r"(ret) : "r"(id), "r"(arg1), "r"(arg2) : "rax", "rbx", "rdx")
+#define SYSCALL3(ret, id, arg1, arg2, arg3) __asm volatile("MOV %1, %%RAX; MOV %2, %%RBX; MOV %3, %%RDX; MOV %4, %%RSI; SYSCALL; MOV %%RAX, %0" : "=r"(ret) : "r"(id), "r"(arg1), "r"(arg2), "r"(arg3) :"rax", "rbx", "rdx", "rsi")
 
 #endif
