9cac708525efee02bce0a26a3886539d84f224b8
Quentin Bergougnoux <quentin.bergougnoux@gmail.com>
2024-09-02 09:36:55 +0200

    fix erroneous merge

--- kern/include/kconfig.h
+++ kern/include/kconfig.h
@@ -6,8 +6,4 @@
  */
 #define CONFIG_ENABLE_MBR 1
 #define CONFIG_ENABLE_ATA 1
-<<<<<<< HEAD
 #define CONFIG_INIT_PROCESS "B:/init"
-=======
-#define CONFIG_INIT_PROCESS "C:/bin/init"
->>>>>>> 8edf8e65148afdd040c456d8bd86e6ada1550a62
